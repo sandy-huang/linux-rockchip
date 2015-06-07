753cc02e7e4517eb12234006239ca1fe2acac9f9
Heiko Stuebner <heiko@sntech.de>
2015-06-07 23:01:08 +0200

    ARM64: kernel: psci: use restart_handlers instead of arm_pm_restart
    Instead of hogging the arm_pm_restart callback, register a restart_handler
    to make it possible for machines to register more board-specific
    restart functionality.
    
    The priority is set to 127, 1 below the "default" to facilitate for
    example the use of regular per-soc restart handlers at their default
    priority 128 and others like the gpio-restart at priority 129 or above.
    
    Non-psci restarts can be necessary when either the psci implementation
    is faulty and does not implement the restart callback or devices need
    even more custom restart operations, like recent rk3288-chromebooks.
    While the soc-level restart could restart those, an external component
    needed to be also reset (via gpio-restart) to allow the device to even
    boot again.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- arch/arm64/kernel/psci.c
+++ arch/arm64/kernel/psci.c
@@ -252,10 +252,17 @@ static int get_set_conduit_method(struct device_node *np)
 	return 0;
 }
 
-static void psci_sys_reset(enum reboot_mode reboot_mode, const char *cmd)
+static int psci_sys_reset(struct notifier_block *this,
+				   unsigned long mode, void *cmd)
 {
 	invoke_psci_fn(PSCI_0_2_FN_SYSTEM_RESET, 0, 0, 0);
+	return NOTIFY_DONE;
 }
+
+static struct notifier_block psci_restart_handler = {
+	.notifier_call = psci_sys_reset,
+	.priority = 127,
+};
 
 static void psci_sys_poweroff(void)
 {
@@ -321,7 +328,7 @@ static void __init psci_0_2_set_functions(void)
 
 	psci_ops.migrate_info_type = psci_migrate_info_type;
 
-	arm_pm_restart = psci_sys_reset;
+	register_restart_handler(&psci_restart_handler);
 
 	pm_power_off = psci_sys_poweroff;
 }
